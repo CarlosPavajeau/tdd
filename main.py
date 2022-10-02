f57b198264984556eb32afc1e87309752f8d0105
Carlos Pavajeau <cantte098@gmail.com>
2022-10-02 15:27:10 -0500

    feat: add hello endpoint

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/hello")
 async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+  return "Hello FastAPI"
