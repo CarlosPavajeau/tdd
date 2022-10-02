b30eeeabc000206f2fc2ae18524f0899229b241d
Gsus07 <jesuscafo@15gmail.com>
2022-10-02 17:49:30 -0500

    feat: add fibonacci functionality

--- test_main.py
+++ test_main.py
@@ -14,7 +14,7 @@ def test_hello_endpoint():
 def test_is_prime_endpoint():
     response = client.get("/is_prime/5")
     assert response.status_code == 200
-    
+
     result = response.json()
     assert result is True
 
@@ -23,3 +23,26 @@ def test_fibonacci_endpoint():
     response = client.get("/fibonacci/1")
     assert response.status_code == 200
     assert response.json() == 1
+
+    response = client.get("/fibonacci/2")
+    assert response.status_code == 200
+    assert response.json() == 1
+
+    response = client.get("/fibonacci/3")
+    assert response.status_code == 200
+    assert response.json() == 2
+
+    response = client.get("/fibonacci/15")
+    assert response.status_code == 200
+    assert response.json() == 610
+
+    response = client.get("/fibonacci/17")
+    assert response.status_code == 200
+    assert response.json() == 1597
+
+    response = client.get("/fibonacci/n")
+    assert response.status_code == 422
+
+    response = client.get("/fibonacci/")
+    assert response.status_code == 404
+
