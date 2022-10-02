253c6c24a09ba1871f0f379d6b14fba6031a4135
Luis Moreno <luifer0115@gmail.com>
2022-10-02 16:56:47 -0500

    feat: add new test case

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ async def is_prime(number: int):
         return True
     elif number % 2 == 0 or number % 3 == 0:
         return False
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i = i + 6
     return True
 
 
--- test_main.py
+++ test_main.py
@@ -36,6 +36,24 @@ def test_is_prime_endpoint():
     result = response.json()
     assert result is True
 
+    response = client.get("/is_prime/49")
+    assert response.status_code == 200
+
+    result = response.json()
+    assert result is False
+
+    response = client.get("/is_prime/567")
+    assert response.status_code == 200
+
+    result = response.json()
+    assert result is False
+
+    response = client.get("/is_prime/97")
+    assert response.status_code == 200
+
+    result = response.json()
+    assert result is True
+
 
 def test_fibonacci_endpoint():
     response = client.get("/fibonacci/1")
