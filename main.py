a28f0dff0de36c80c39bb867f05a13f4c90c2363
Carlos Pavajeau <cantte098@gmail.com>
2022-10-02 15:31:35 -0500

    feat: add initial endpoints

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ app = FastAPI()
 
 @app.get("/hello")
 async def root():
-  return "Hello FastAPI"
+    return "Hello FastAPI"
+
+
+@app.get("is_prime/{number}")
+async def is_prime(number: int):
+    return True
+
+
+@app.get("fibonacci/{number}")
+async def fibonacci(number: int):
+    return 1
